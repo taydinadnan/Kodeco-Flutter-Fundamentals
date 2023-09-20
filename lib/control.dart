1b0bcb4336aed8de8b49c68ba99e6868987ffb85
Adnan Turgay Aydin <taydinadnan@gmail.com>
2023-09-20 11:53:14 +0200

    Customizing slider thumb with an image

--- lib/control.dart
+++ lib/control.dart
@@ -1,5 +1,9 @@
+import 'dart:ui' as ui;
+
 import 'package:bullseye/game_model.dart';
+import 'package:bullseye/slider_thumb_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Control extends StatefulWidget {
   const Control({super.key, required this.model});
@@ -11,6 +15,25 @@ class Control extends StatefulWidget {
 }
 
 class _ControlState extends State<Control> {
+  ui.Image? _sliderImage;
+
+  Future<ui.Image> _load(String asset) async {
+    final data = await rootBundle.load(asset);
+    final codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    final frame = await codec.getNextFrame();
+    return frame.image;
+  }
+
+  @override
+  void initState() {
+    _load('images/nub.png').then((image) {
+      setState(() {
+        _sliderImage = image;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -33,6 +56,7 @@ class _ControlState extends State<Control> {
               thumbColor: Colors.redAccent,
               overlayColor: Colors.red.withAlpha(32),
               overlayShape: const RoundSliderOverlayShape(overlayRadius: 28.0),
+              thumbShape: SliderThumbImage(_sliderImage),
             ),
             child: Slider(
               value: widget.model.current.toDouble(),
