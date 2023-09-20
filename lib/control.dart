34a001509477d8272068a7284508e09b6f36f749
Adnan Turgay Aydin <taydinadnan@gmail.com>
2023-09-20 11:39:21 +0200

    Customizing slider and fixing padding.

--- lib/control.dart
+++ lib/control.dart
@@ -24,15 +24,26 @@ class _ControlState extends State<Control> {
           ),
         ),
         Expanded(
-          child: Slider(
-            value: widget.model.current.toDouble(),
-            onChanged: (newValue) {
-              setState(() {
-                widget.model.current = newValue.toInt();
-              });
-            },
-            max: 100.0,
-            min: 1.0,
+          child: SliderTheme(
+            data: SliderTheme.of(context).copyWith(
+              activeTrackColor: Colors.red[700],
+              inactiveTrackColor: Colors.red[700],
+              trackShape: const RoundedRectSliderTrackShape(),
+              trackHeight: 8.0,
+              thumbColor: Colors.redAccent,
+              overlayColor: Colors.red.withAlpha(32),
+              overlayShape: const RoundSliderOverlayShape(overlayRadius: 28.0),
+            ),
+            child: Slider(
+              value: widget.model.current.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  widget.model.current = newValue.toInt();
+                });
+              },
+              max: 100.0,
+              min: 1.0,
+            ),
           ),
         ),
         const Padding(
--- lib/score.dart
+++ lib/score.dart
@@ -22,19 +22,21 @@ class Score extends StatelessWidget {
           icon: Icons.refresh,
           onPressed: onStartOver,
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 32.0, right: 32.0),
-          child: Column(
-            children: [
-              Text(
-                "Score: ",
-                style: LabelTextStyle.bodyText1(context),
-              ),
-              Text(
-                '$totalScore',
-                style: ScoreNumberTextStyle.headline4(context),
-              ),
-            ],
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 32.0, right: 32.0),
+            child: Column(
+              children: [
+                Text(
+                  "Score: ",
+                  style: LabelTextStyle.bodyText1(context),
+                ),
+                Text(
+                  '$totalScore',
+                  style: ScoreNumberTextStyle.headline4(context),
+                ),
+              ],
+            ),
           ),
         ),
         Padding(
